0392ef46b9d69e0b9d809f3a5fdd93bcae36cbae
Bikram <bikramrout24@gmail.com>
2020-12-21 10:47:22 +0530

    final code

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -9,16 +9,25 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class FooterComponent implements OnInit {
 
   durationInSeconds = 5;
+  @ViewChild('email') inputElement: ElementRef;
   constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
   subscribe(): void {
-    this.snackBar.open('You are subscribed to our mailing list, Stay tuned for the updated',
-      null, {
-      duration: 2000,
-    });
+    if (this.inputElement.nativeElement.value) {
+
+      this.snackBar.open('You are subscribed to our mailing list, Stay tuned for the updated',
+        null, {
+        duration: 2000,
+      });
+    } else {
+      this.snackBar.open('Please enter valid email address to subscribe our mailing list',
+        null, {
+        duration: 2000,
+      });
+    }
   }
 
 }
